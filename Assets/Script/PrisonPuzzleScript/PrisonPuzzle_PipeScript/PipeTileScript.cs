924a25f0f405d9a4a240811498c69288278e9dd5
acastracity <angkr2420@gmail.com>
2024-10-28 18:26:19 +0900

    [Feature] 파이프 타일 회전 함수 구현
    - 파이프 타일 회전 각도 변수를 추가했습니다.
    - 파이프 타일 회전 함수 RotateTile()을 구현했습니다.
    - 마우스 좌클릭 시 회전하는 함수를 구현했습니다.

--- Assets/Script/PrisonPuzzleScript/PrisonPuzzle_PipeScript/PipeTileScript.cs
+++ Assets/Script/PrisonPuzzleScript/PrisonPuzzle_PipeScript/PipeTileScript.cs
@@ -4,4 +4,29 @@ using UnityEngine;
 
 public class PipeTileScript : MonoBehaviour
 {
+    // 회전 각도 (0, 90, 180, 270도로 제한)
+    private int currentRotation;
+
+    // 스프라이트 렌더러
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        currentRotation = 0; // 초기 회전 각도
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void RotateTile()
+    {
+        // 90도씩 시계방향 회전
+        currentRotation = (currentRotation + 90) % 360;
+        // Z축을 기준으로 회전
+        transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+    }
+
+    void OnMouseDown()
+    {
+        RotateTile();
+    }
+
 }
