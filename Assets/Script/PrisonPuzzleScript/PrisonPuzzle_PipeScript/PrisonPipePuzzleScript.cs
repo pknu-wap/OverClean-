14eb508c6bda41a819ab5653ad17f809c5314c1b
acastracity <angkr2420@gmail.com>
2024-10-30 20:05:33 +0900

    [Feature] 파이프 타일 생성 시, 모양 및 회전 정보 저장
    - 파이프 타일 생성 시에, 생성한 타일의 정보를 각 타일에 저장하려 합니다.
    - PrisonPipePuzzleScript.cs 에서 PipeTileScript.cs로 파이프 모양 및 회전 정보를 전달하였습니다.

--- Assets/Script/PrisonPuzzleScript/PrisonPuzzle_PipeScript/PrisonPipePuzzleScript.cs
+++ Assets/Script/PrisonPuzzleScript/PrisonPuzzle_PipeScript/PrisonPipePuzzleScript.cs
@@ -8,6 +8,7 @@ public class PrisonPipePuzzleScript : MonoBehaviour
 {
     // Grid Layout Group을 포함한 빈 오브젝트
     public GameObject pipeGrid;
+    
     // 파이프 모양 배열 (일자, L자, T자)
     public GameObject[] pipeShapes;
     
@@ -17,6 +18,7 @@ public class PrisonPipePuzzleScript : MonoBehaviour
     // 파이프 타일 생성의 초기 x,y 좌표
     private int originXPosition = -494;
     private int originYPosition = 216;
+
     // 각 타일 간격 (약간의 여유 공간 포함)
     private int tileSize = 110;
 
@@ -31,15 +33,21 @@ public class PrisonPipePuzzleScript : MonoBehaviour
         {
             for (int x = 0; x < gridWidth; x++)
             {
-                // 랜덤한 파이프 프리팹 선택
-                int randomIndex = Random.Range(0, pipeShapes.Length);
-                GameObject pipeTile = Instantiate(pipeShapes[randomIndex], pipeGrid.transform);
+                // 랜덤한 파이프 프리팹 모양 선택
+                int randomShape = Random.Range(0, pipeShapes.Length);
+                GameObject pipeTile = Instantiate(pipeShapes[randomShape], pipeGrid.transform);
 
                 // 타일 위치 계산
                 Vector3 tilePosition = new Vector3(originXPosition + x * tileSize, originYPosition -y * tileSize, 0);
                 pipeTile.transform.localPosition = tilePosition;
                 // 0도, 90도, 180도, 270도 중 하나로 회전
-                pipeTile.transform.localRotation = Quaternion.Euler(0, 0, Random.Range(0, 4) * 90);
+                int randomRotation = Random.Range(0, 4) * 90;
+                pipeTile.transform.localRotation = Quaternion.Euler(0, 0, randomRotation);
+
+                // 각 파이프 타일의 스크립트에 pipeShape를 전해주기 위해 GetComponent 사용
+                pipeTile.GetComponent<PipeTileScript>().pipeShape = randomShape;
+                // 각 파이프 타일의 스크립트에 currentRotation을 전해주기 위해 GetComponent 사용
+                pipeTile.GetComponent<PipeTileScript>().currentRotation = randomRotation;
             }
         }
     }
