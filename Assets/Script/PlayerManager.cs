1630ae7c18e33659506ed87f77f8d2a7305b52dc
Do Hyeon Seok <dohyeondol@gmail.com>
2024-11-13 02:47:47 +0900

    [Feature] 플레이어 이동 동기화
    - 로컬 플레이어와 원격 플레이어를 구분하여 데이터를 받고, 전송하는 기능을 추가하였습니다.

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviourPun
+public class PlayerManager : MonoBehaviourPun, IPunObservable
 {
     public Vector2 inputVec1;
     public Vector2 inputVec2;
     public float speed;
     public bool canMove = true;
-    
+
     private Rigidbody2D rigid;
     private SpriteRenderer spriter;
     private Animator anim;
@@ -18,6 +18,11 @@ public class PlayerManager : MonoBehaviourPun
     public int playerID;
     private PhotonView photonView;
 
+    // 네트워크 동기화용 변수
+    private Vector2 networkedPosition;
+    private float networkedSpeed;
+    private int networkedDirection;
+
     void Start()
     {
         photonView = GetComponent<PhotonView>();
@@ -25,10 +30,13 @@ public class PlayerManager : MonoBehaviourPun
         spriter = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-        // 로컬 플레이어가 아니라면, 스크립트 비활성화
+        // 초기 위치 설정
+        networkedPosition = rigid.position;
+        
+        // 로컬 플레이어가 아니라면, 물리 상호작용 비활성화
         if (!photonView.IsMine)
         {
-            enabled = false;
+            rigid.isKinematic = true;
         }
     }
 
@@ -72,16 +80,9 @@ public class PlayerManager : MonoBehaviourPun
         // 로컬 플레이어가 아닌 경우 애니메이션 처리하지 않음
         if (!photonView.IsMine) return;
 
-        if (playerID == 1)
-        {
-            anim.SetFloat("Speed", inputVec1.magnitude);
-            UpdateAnimationDirection(inputVec1);
-        }
-        else if (playerID == 2)
-        {
-            anim.SetFloat("Speed", inputVec2.magnitude);
-            UpdateAnimationDirection(inputVec2);
-        }
+        Vector2 currentInputVec = (playerID == 1) ? inputVec1 : inputVec2;
+        anim.SetFloat("Speed", currentInputVec.magnitude);
+        UpdateAnimationDirection(currentInputVec);
     }
 
     private void UpdateAnimationDirection(Vector2 inputVec)
@@ -101,6 +102,32 @@ public class PlayerManager : MonoBehaviourPun
         else if (inputVec.y > 0)
         {
             anim.SetInteger("Direction", 3); // 뒤쪽
+        }
+    }
+
+    // IPunObservable 인터페이스 구현을 통한 네트워크 동기화
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // 로컬 플레이어의 데이터를 전송
+            stream.SendNext(rigid.position);
+            stream.SendNext(anim.GetFloat("Speed"));
+            stream.SendNext(anim.GetInteger("Direction"));
+        }
+        else
+        {
+            // 원격 플레이어의 데이터를 수신
+            networkedPosition = (Vector2)stream.ReceiveNext();
+            networkedSpeed = (float)stream.ReceiveNext();
+            networkedDirection = (int)stream.ReceiveNext();
+
+            // 수신한 값을 애니메이션에 반영
+            anim.SetFloat("Speed", networkedSpeed);
+            anim.SetInteger("Direction", networkedDirection);
+
+            // 원격 플레이어의 위치를 보간 이동
+            rigid.position = Vector2.Lerp(rigid.position, networkedPosition, Time.deltaTime * 10);
         }
     }
 }
