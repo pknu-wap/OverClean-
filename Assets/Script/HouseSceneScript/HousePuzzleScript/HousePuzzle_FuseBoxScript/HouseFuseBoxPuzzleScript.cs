230424d61448b21715cb7cee7bc5b057cfb54046
고혁빈 <rhgurqls00@gmail.com>
2024-11-16 07:44:32 +0900

    [Feature] 전자자물쇠 퍼즐 구현

--- Assets/Script/HouseSceneScript/HousePuzzleScript/HousePuzzle_FuseBoxScript/HouseFuseBoxPuzzleScript.cs
+++ Assets/Script/HouseSceneScript/HousePuzzleScript/HousePuzzle_FuseBoxScript/HouseFuseBoxPuzzleScript.cs
@@ -44,6 +44,8 @@ public class HouseFuseBoxPuzzleScript : MonoBehaviour
             yield return StartCoroutine(ShowImage(oImage));
             // puzzlesuccess 호출
             PuzzleManager.instance.PuzzleSuccess();
+            // 퓨즈 퍼즐 해결 상태 갱신
+            StageManager.fusePuzzleSolved = true;
             // 제거할 프리팹 없으니 바로 씬 닫기
             SceneManager.UnloadSceneAsync("HouseFuseBoxPuzzleScene");
         }
