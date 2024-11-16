15e33cf0f5da87b414953eb8268ebebe850df893
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-16 22:22:38 +0900

    [Featrue] 씬이 바뀌더라도 배경음악이 안꺼지고 계속 켜짐.

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static AudioManager instance;
+    // 씬별 음악 리스트
+    public AudioClip[] backgroundMusic; 
+    private AudioSource audioSource;
+
+    void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+void Start()
     {
-        
+        // 배경음악 자동 재생
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
+
+    public void SetVolume(float volume)
+    {
+        // 볼륨 조절
+        audioSource.volume = volume; 
+    }
+
+    // 오디오의 현재 재생 위치 가져오기
+    public float GetCurrentTime()
+    {
+        return audioSource.time;
+    }
+
+    // 오디오의 특정 위치에서 재생 시작
+    public void SetCurrentTime(float time)
+    {
+        audioSource.time = time;
+    }
+
 }
