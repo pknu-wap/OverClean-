5bb99a83e36258c3929ba9b2b41df54d318c3bc6
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-24 19:47:47 +0900

    [Feature] ESC눌렀을때 일시정지창 나오게 구현

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -6,18 +6,28 @@ using Photon.Pun;
 public class UIManager : MonoBehaviour
 {
     public RectTransform tutorialPanel;
+    
+    // 버튼이 있는 일시정지 창 -> 로비로 돌아가기, 게임으로 돌아가기
     public RectTransform pausePanel;
+
+    // 버튼이 없는 일시정지 창 "일시정지됨"
+    public RectTransform pauseTextPanel;
     public RectTransform titleExitPanel;
     public RectTransform roomCodeInputPanel;
     public bool tutorialPanelOpen = true;
     public bool titleExitPanelOpen = false;
     public bool roomCodeInputPanelOpen = false;
-    public bool pause = false;
+    public bool isPaused = false;
+    private PhotonView photonView;
+
+    // 일시정지를 시작한 플레이어 ID 초기화 한거 같던데
+    private int pausedByPlayerId = -1;   
 
     // 기존에 있던 싱글톤 패턴을 해제하고 씬마다의 UiManager에게 공통 스크립트를 부여, 해당 씬에 있는 버튼만 사용하게 합니다
     // 앞으로 추가되는 버튼들도 이곳에 추가 / 할당한 후 사용하되, 절대 그 씬에 존재하지 않는 버튼을 사용하는 로직을 추가하지 마세요! 바로 오류납니다
     private void Start()
     {
+        photonView = GetComponent<PhotonView>();
         if(SceneManager.GetActiveScene().name == "PrisonScene" || SceneManager.GetActiveScene().name == "HouseScene")
         {
             tutorialPanel.gameObject.SetActive(tutorialPanelOpen);
@@ -28,17 +38,19 @@ public class UIManager : MonoBehaviour
     {
         if(SceneManager.GetActiveScene().name == "PrisonScene" || SceneManager.GetActiveScene().name == "HouseScene")
         {
-            if(tutorialPanelOpen && !pause && Input.GetKeyDown(KeyCode.Escape))
+            if(tutorialPanelOpen && !isPaused && Input.GetKeyDown(KeyCode.Escape))
             {
                 CloseTutorialPanel();
             }
-            else if(!tutorialPanelOpen && !pause && Input.GetKeyDown(KeyCode.Escape))
+            else if(!tutorialPanelOpen && !isPaused && Input.GetKeyDown(KeyCode.Escape))
             {
-                OpenEscPanel();
+                pausedByPlayerId = PhotonNetwork.LocalPlayer.ActorNumber;
+                photonView.RPC("UpdatePauseState", RpcTarget.All, pausedByPlayerId, true);
             }
-            else if(!tutorialPanelOpen && pause && Input.GetKeyDown(KeyCode.Escape))
+            else if(!tutorialPanelOpen && isPaused && Input.GetKeyDown(KeyCode.Escape) && pausedByPlayerId == PhotonNetwork.LocalPlayer.ActorNumber)
             {
-                CloseEscPanel();
+                pausedByPlayerId = PhotonNetwork.LocalPlayer.ActorNumber;
+                photonView.RPC("UpdatePauseState", RpcTarget.All, pausedByPlayerId, false);
             }
         }
         else if(SceneManager.GetActiveScene().name == "TitleScene")
@@ -84,18 +96,58 @@ public class UIManager : MonoBehaviour
         tutorialPanel.gameObject.SetActive(tutorialPanelOpen);
     }
 
-    public void OpenEscPanel()
+    [PunRPC]
+    void UpdatePauseState(int actorNumber, bool pauseState)
     {
-        pause = true;
-        Time.timeScale = 0;
-        pausePanel.gameObject.SetActive(true);
+        // 얘가 지금 하고 있는건가? 
+        isPaused = pauseState;
+        // 
+        if (pauseState)
+        {
+            // 모든 플레이어에서 시간 멈춤
+            Time.timeScale = 0;
+            // pausedByPlayerId 모든 클라이언트에서 재설정
+            pausedByPlayerId = actorNumber;
+            // 일시정지가 활성화된 경우
+            if(PhotonNetwork.LocalPlayer.ActorNumber == actorNumber)
+            {
+                // 자신이 일시정지를 시작한 경우
+                pausePanel.gameObject.SetActive(true);
+                pauseTextPanel.gameObject.SetActive(false);
+            }
+            if(PhotonNetwork.LocalPlayer.ActorNumber != actorNumber)
+            {
+                // 다른 플레이어가 일시정지를 시작한 경우
+                pausePanel.gameObject.SetActive(false);
+                pauseTextPanel.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Time.timeScale = 1;
+            if (PhotonNetwork.LocalPlayer.ActorNumber == actorNumber)
+            {
+                // 자신이 일시정지를 시작한 경우
+                pausePanel.gameObject.SetActive(false);
+                pauseTextPanel.gameObject.SetActive(false);
+            }
+            if(PhotonNetwork.LocalPlayer.ActorNumber != actorNumber)
+            {
+                // 다른 플레이어가 일시정지를 시작한 경우
+                pausePanel.gameObject.SetActive(false);
+                pauseTextPanel.gameObject.SetActive(false);
+            }
+        }
+        
     }
-    
-    public void CloseEscPanel()
+
+public void ClosePausePanel()
     {
-        pause = false;
-        Time.timeScale = 1;
-        pausePanel.gameObject.SetActive(false);
+        if(isPaused && pausedByPlayerId == PhotonNetwork.LocalPlayer.ActorNumber)
+        {
+            // 모든 클라이언트에 상태 동기화
+            photonView.RPC("UpdatePauseState", RpcTarget.All, pausedByPlayerId, false);
+        }
     }
 
     public void LoadMapChooseScene()
@@ -144,7 +196,7 @@ public class UIManager : MonoBehaviour
     // MapClearPanel -> BackToLobbyButton
     public void LoadLobbyScene()
     {
-        SceneManager.LoadScene("LobbyScene");
+        PhotonNetwork.LoadLevel("LobbyScene"); 
     } 
 
 }
