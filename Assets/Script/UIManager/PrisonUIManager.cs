a889440b7d7d787a75d902149539784dd91571d7
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-25 19:50:42 +0900

    [Refactor] UIManager -> PrisonUIManager로 변경 + 자잘한 주석 제거 + 리펙토링

--- Assets/Script/UIManager/PrisonUIManager.cs
+++ Assets/Script/UIManager/PrisonUIManager.cs
@@ -2,41 +2,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 
-public class UIManager : MonoBehaviour
+public class PrisonUIManager : MonoBehaviour
 {
-    // 스테이지 매니저 참조
     public StageManager stageManager;
-    public RectTransform tutorialPanel;
-    
-    // 버튼이 있는 일시정지 창 -> 로비로 돌아가기, 게임으로 돌아가기
-    public RectTransform pausePanel;
-
-    // 버튼이 없는 일시정지 창 "일시정지됨"
-    public RectTransform pauseTextPanel;
-    public bool tutorialPanelOpen = true;
-    public bool isPaused = false;
     private PhotonView photonView;
 
-    // 일시정지를 시작한 플레이어 ID 초기화 한거 같던데
+    public GameObject tutorialPanel;
+    public GameObject pausePanel;
+    public GameObject pauseTextPanel;
+
+    public bool tutorialPanelOpen = true;
+    public bool isPaused = false;
+
     public int pausedByPlayerId = -1;   
 
-    // 기존에 있던 싱글톤 패턴을 해제하고 씬마다의 UiManager에게 공통 스크립트를 부여, 해당 씬에 있는 버튼만 사용하게 합니다
-    // 앞으로 추가되는 버튼들도 이곳에 추가 / 할당한 후 사용하되, 절대 그 씬에 존재하지 않는 버튼을 사용하는 로직을 추가하지 마세요! 바로 오류납니다
     private void Start()
     {
         stageManager = FindObjectOfType<StageManager>();
         photonView = GetComponent<PhotonView>();
-        if(SceneManager.GetActiveScene().name == "PrisonScene" || SceneManager.GetActiveScene().name == "HouseScene")
-        {
-            tutorialPanel.gameObject.SetActive(tutorialPanelOpen);
-        }
+        tutorialPanel.SetActive(tutorialPanelOpen);
     }
 
     private void Update()
     {
-        if(SceneManager.GetActiveScene().name == "PrisonScene" || SceneManager.GetActiveScene().name == "HouseScene")
-        {
-            if(tutorialPanelOpen && !isPaused && Input.GetKeyDown(KeyCode.Escape))
+        if(tutorialPanelOpen && !isPaused && Input.GetKeyDown(KeyCode.Escape))
             {
                 CloseTutorialPanel();
             }
@@ -50,10 +39,8 @@ public class UIManager : MonoBehaviour
                 pausedByPlayerId = PhotonNetwork.LocalPlayer.ActorNumber;
                 photonView.RPC("UpdatePauseState", RpcTarget.All, pausedByPlayerId, false);
             }
-        } 
     }
 
-    // Prison & House
     public void OpenTutorialPanel()
     {
         if(!tutorialPanelOpen)
@@ -76,46 +63,35 @@ public class UIManager : MonoBehaviour
 
     public void ClosePausePanel()
     {
-        // 모든 클라이언트에 상태 동기화
         photonView.RPC("UpdatePauseState", RpcTarget.All, pausedByPlayerId, false);
     }
 
     [PunRPC]
     void UpdatePauseState(int actorNumber, bool pauseState)
     {
-        // 일시정지 상태 동기화
         isPaused = pauseState;
         if (pauseState)
         {
-            // 모든 플레이어에서 시간 멈춤
             stageManager.isPaused = true;
-            // 모든 플레이어 이동 제한
             stageManager.SetPlayerMovement(false);
-            // pausedByPlayerId 모든 클라이언트에서 재설정
             pausedByPlayerId = actorNumber;
-            // 일시정지가 활성화된 경우
+
             if (PhotonNetwork.LocalPlayer.ActorNumber == actorNumber)
             {
-                // 자신이 일시정지를 시작한 경우
                 pausePanel.gameObject.SetActive(true);
                 pauseTextPanel.gameObject.SetActive(false);
             }
             else
             {
-                // 다른 플레이어가 일시정지를 시작한 경우
                 pausePanel.gameObject.SetActive(false);
                 pauseTextPanel.gameObject.SetActive(true);
             }
         }
         else if(!pauseState)
         {
-            // 일시정지 해제
             stageManager.isPaused = false;
-            // 이동 활성화
             stageManager.SetPlayerMovement(true);
-            // pausedByPlayerId 모든 클라이언트에서 재설정
             pausedByPlayerId = -1;
-            // 다른 플레이어가 일시정지를 시작한 경우
             pausePanel.gameObject.SetActive(false);
             pauseTextPanel.gameObject.SetActive(false);
         }
@@ -127,19 +103,12 @@ public class UIManager : MonoBehaviour
         PhotonNetwork.LoadLevel("MapChooseScene"); 
     }
     
-    // TitleScene -> StartButton
-    // MapClearPanel -> BackToLobbyButton
     public void LoadLobbyScene()
-    {
-        PhotonNetwork.LoadLevel("LobbyScene"); 
-    } 
-    
-    [PunRPC]
-    public void ExitAndGotoLobbySceneButtonClick()
     {
         photonView.RPC("ExitAndGotoLobbyScene", RpcTarget.All);
     }
 
+    [PunRPC]
     public void ExitAndGotoLobbyScene()
     {
         PhotonNetwork.LoadLevel("LobbyScene"); 
