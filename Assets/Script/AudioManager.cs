5aee3ec5724c43d6e4ee4612fd683560a1e3b9f3
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-26 00:17:42 +0900

    [Feature] Title -> Lobby -> Room씬 이동해도 배경음악 계속 흐름

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -6,49 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-    private static AudioManager instance;
-    // 씬별 음악 리스트
-    public AudioClip[] backgroundMusic; 
-    private AudioSource audioSource;
+    public AudioSource bgm;
 
-    void Awake()
+    private void Awake()
     {
-        if (instance != null && instance != this)
+        var soundManangers = FindObjectsOfType<AudioManager>();
+        if(soundManangers.Length == 1)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
         {
             Destroy(gameObject);
-            return;
-        }
-
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-        audioSource = GetComponent<AudioSource>();
-    }
-
-void Start()
-    {
-        // 배경음악 자동 재생
-        if (!audioSource.isPlaying)
-        {
-            audioSource.Play();
         }
     }
-
-    public void SetVolume(float volume)
+    void Start()
     {
-        // 볼륨 조절
-        audioSource.volume = volume; 
+        bgm.Play();
     }
-
-    // 오디오의 현재 재생 위치 가져오기
-    public float GetCurrentTime()
-    {
-        return audioSource.time;
-    }
-
-    // 오디오의 특정 위치에서 재생 시작
-    public void SetCurrentTime(float time)
-    {
-        audioSource.time = time;
-    }
-
 }
