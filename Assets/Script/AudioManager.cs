c5cac5465a3095e8df19b486cb8ec08da9111ea4
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-26 00:37:00 +0900

    [Feature] PrisonScene으로 넘어가면 배경음악이 바뀜

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class AudioManager : MonoBehaviour
 {
     public AudioSource bgm;
+    public AudioClip defaultMusic;
+    public AudioClip prisonMusic;
 
     private void Awake()
     {
@@ -20,9 +22,42 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void OnEnable()
+    {
+        // 씬 로드 이벤트 등록
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // 씬 로드 이벤트 해제
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
     void Start()
     {
-        bgm.loop = true;
+        bgm.loop = true; // 기본적으로 반복 재생
+        bgm.clip = defaultMusic; // 기본 음악 설정
+        bgm.Play();
+    }
+     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "PrisonScene") // PrisonScene일 경우
+        {
+            ChangeMusic(prisonMusic);
+        }
+        else // 다른 씬으로 돌아왔을 경우
+        {
+            ChangeMusic(defaultMusic);
+        }
+    }
+
+    private void ChangeMusic(AudioClip newMusic)
+    {
+        if (bgm.clip == newMusic) return; // 이미 해당 음악이 재생 중이면 변경하지 않음
+
+        bgm.Stop();
+        bgm.clip = newMusic;
         bgm.Play();
     }
 }
