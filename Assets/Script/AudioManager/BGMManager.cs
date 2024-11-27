01253b0ee3404daef0c1102533fcefa74dbbeaa2
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-27 22:18:43 +0900

    [Refactor] AudioManager를 BGM과 SoundAsset을 관리하는 매니저로 따로 분리함

--- Assets/Script/AudioManager/BGMManager.cs
+++ Assets/Script/AudioManager/BGMManager.cs
@@ -4,20 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class AudioManager : MonoBehaviour
+public class BGMManager : MonoBehaviour
 {
     public AudioSource bgm;
     public AudioClip defaultMusic;
     public AudioClip prisonMusic;
-    public AudioSource sfxSource;
-    public List<AudioClip> prisonLockSounds;
-    public AudioClip prisonDoorOpenSound;
-    public AudioClip prisonDustLeafSound;
-    public AudioClip buttonClickSound;
-
     private void Awake()
     {
-        var soundManangers = FindObjectsOfType<AudioManager>();
+        var soundManangers = FindObjectsOfType<BGMManager>();
         if(soundManangers.Length == 1)
         {
             DontDestroyOnLoad(gameObject);
@@ -47,7 +41,7 @@ public class AudioManager : MonoBehaviour
         bgm.clip = defaultMusic; 
         bgm.Play();
     }
-     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         // 다른 씬으로 돌아왔을 경우
         if (scene.name == "TitleScene" && scene.name == "LobbyScene" && scene.name == "RoomScene")
@@ -76,41 +70,5 @@ public class AudioManager : MonoBehaviour
         bgm.Stop();
         bgm.clip = newMusic;
         bgm.Play();
-    }
-
-    // 랜덤 효과음 재생 메서드
-    public void PlayRandomPrisonDoorPuzzleKeySound()
-    {
-        if (prisonLockSounds.Count == 0) 
-        {
-            Debug.Log("PrisonLockSounds.Count == 0");   
-            return;
-        }
-        AudioClip randomClip = prisonLockSounds[Random.Range(0, prisonLockSounds.Count)];
-        float volumeScale = 3.0f;
-        // 열쇠소리를 더 크게 하고자 함.
-        sfxSource.PlayOneShot(randomClip, volumeScale);
-        Debug.Log("PrisonLockSound : 정상적으로 사운드가 재생되었습니다!");
-    }
-
-    public void PrisonDoorOpenSound()
-    {  
-        float volumeScale = 3.0f;
-        sfxSource.PlayOneShot(prisonDoorOpenSound, volumeScale);
-        Debug.Log("PrisonDoorOpen : 정상적으로 사운드가 재생되었습니다!");
-    }
-
-    public void PrisonDustLeafRustingSound()
-    {
-        float volumeScale = 3.0f;
-        sfxSource.PlayOneShot(prisonDustLeafSound,volumeScale);
-        Debug.Log("PrisonDustLeafRusting : 정상적으로 사운드가 재생되었습니다!");
-    }
-
-    public void ButtonClickSound()
-    {
-        float volumeScale = 3.0f;
-        sfxSource.PlayOneShot(buttonClickSound, volumeScale);
-        Debug.Log("ButtonClickSound : 정상적으로 사운드가 재생되었습니다!");
     }
 }
---
+++ Assets/Script/AudioManager/SoundAssetManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource sfxSource;
+    public List<AudioClip> prisonLockSounds;
+    public AudioClip prisonDoorOpenSound;
+    public AudioClip prisonDustLeafSound;
+    public AudioClip buttonClickSound;
+
+    // 랜덤 효과음 재생 메서드
+    public void PlayRandomPrisonDoorPuzzleKeySound()
+    {
+        if (prisonLockSounds.Count == 0) 
+        {
+            Debug.Log("PrisonLockSounds.Count == 0");   
+            return;
+        }
+        AudioClip randomClip = prisonLockSounds[Random.Range(0, prisonLockSounds.Count)];
+        float volumeScale = 3.0f;
+        // 열쇠소리를 더 크게 하고자 함.
+        sfxSource.PlayOneShot(randomClip, volumeScale);
+        Debug.Log("PrisonLockSound : 정상적으로 사운드가 재생되었습니다!");
+    }
+
+    public void PrisonDoorOpenSound()
+    {  
+        float volumeScale = 3.0f;
+        sfxSource.PlayOneShot(prisonDoorOpenSound, volumeScale);
+        Debug.Log("PrisonDoorOpen : 정상적으로 사운드가 재생되었습니다!");
+    }
+
+    public void PrisonDustLeafRustingSound()
+    {
+        float volumeScale = 3.0f;
+        sfxSource.PlayOneShot(prisonDustLeafSound,volumeScale);
+        Debug.Log("PrisonDustLeafRusting : 정상적으로 사운드가 재생되었습니다!");
+    }
+
+    public void ButtonClickSound()
+    {
+        float volumeScale = 3.0f;
+        sfxSource.PlayOneShot(buttonClickSound, volumeScale);
+        Debug.Log("ButtonClickSound : 정상적으로 사운드가 재생되었습니다!");
+    }
+}
