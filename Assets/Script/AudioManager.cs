12228a4a14bf0d0b60519abae0210089298b6870
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-27 20:07:10 +0900

    [Feature] 감옥 문 퍼즐에서, 열쇠 SoundAsset 적용

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -9,6 +9,8 @@ public class AudioManager : MonoBehaviour
     public AudioSource bgm;
     public AudioClip defaultMusic;
     public AudioClip prisonMusic;
+    public AudioSource sfxSource;
+    public List<AudioClip> prisonLockSounds;
 
     private void Awake()
     {
@@ -36,28 +38,55 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        bgm.loop = true; // 기본적으로 반복 재생
-        bgm.clip = defaultMusic; // 기본 음악 설정
+        // 기본적으로 반복 재생
+        bgm.loop = true; 
+        // 기본 음악 설정
+        bgm.clip = defaultMusic; 
         bgm.Play();
     }
      private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == "PrisonScene") // PrisonScene일 경우
-        {
-            ChangeMusic(prisonMusic);
-        }
-        else // 다른 씬으로 돌아왔을 경우
+        // 다른 씬으로 돌아왔을 경우
+        if (scene.name == "TitleScene" && scene.name == "LobbyScene" && scene.name == "RoomScene")
         {
             ChangeMusic(defaultMusic);
+            bgm.volume = 1.0f;
+        }
+        // PrisonScene일 경우
+        if (scene.name == "PrisonScene") 
+        {
+            if(scene.name == "PrisonDoorPuzzleScene" || 
+            scene.name == "PrisonDustPuzzleScene" || 
+            scene.name == "PrisonLeafPuzzleScene" || 
+            scene.name == "PrisonPipePuzzleScene") 
+            return;
+            bgm.volume = 0.3f;
+            ChangeMusic(prisonMusic);
         }
     }
 
     private void ChangeMusic(AudioClip newMusic)
     {
-        if (bgm.clip == newMusic) return; // 이미 해당 음악이 재생 중이면 변경하지 않음
+        // 이미 해당 음악이 재생 중이면 변경하지 않음
+        if (bgm.clip == newMusic) return; 
 
         bgm.Stop();
         bgm.clip = newMusic;
         bgm.Play();
+    }
+
+    // 랜덤 효과음 재생 메서드
+    public void PlayRandomPrisonDoorPuzzleKeySound()
+    {
+        if (prisonLockSounds.Count == 0) 
+        {
+            Debug.Log("PrisonLockSounds.Count == 0");   
+            return;
+        }
+        AudioClip randomClip = prisonLockSounds[Random.Range(0, prisonLockSounds.Count)];
+        float volumeScale = 3.0f;
+        // 열쇠소리를 더 크게 하고자 함.
+        sfxSource.PlayOneShot(randomClip, volumeScale);
+        Debug.Log("정상적으로 사운드가 재생되었습니다!");
     }
 }
